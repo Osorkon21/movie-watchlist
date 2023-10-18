5e58d4c0c87e3eb8d78a551d1789107559c635d9
Victor Montelongo <victoramontelongo@gmail.com>
2023-10-18 12:13:53 -0500

    storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
 const body = $("body");
 const movieNameInput = $("#movie-name-input");
+const carouselContainer = $(".carousel-container");
 const cardContainer = $(".card-container");
 const submitBtn = $(".submit-btn");
 const searchMovieName = $(".search-movie-name");
+var currentMovieData;
+body.on("click", ".watchlist-btn", onWatchlistBtnClick)
 
+function onWatchlistBtnClick() {
+  saveToLocalStorage(currentMovieData);
+}
 body.on("click", ".submit-btn", onMovieNameInput);
 
-// getTrendingMovies();
+getTrendingMovies();
 
 async function getTrendingMovies() {
   const trendingMovies = `https://api.themoviedb.org/3/trending/movie/day?language=en-US&api_key=6c6fe5f85d17328e8be5488bcb10da64`;
@@ -14,10 +20,7 @@ async function getTrendingMovies() {
   const trendingData = await trendingResponse.json();
 
   console.log(trendingData);
-
-  for (var i = 0; i < trendingData.results.length; i++) {
-    addMovieCard(trendingData.results[i]);
-  }
+  populateCarousel(trendingData.results);
 }
 
 function onMovieNameInput(e) {
@@ -27,6 +30,7 @@ function onMovieNameInput(e) {
 }
 
 async function getMovieInfo(movieName) {
+  currentMovieData = movieData.results[0];
   const movieInfo = `https://api.themoviedb.org/3/search/movie?language=en-US&query=${convertSpaces(movieName)}&api_key=6c6fe5f85d17328e8be5488bcb10da64`;
 
   const movieResponse = await fetch(movieInfo);
@@ -40,7 +44,9 @@ async function getMovieInfo(movieName) {
   if (movieData.results.length !== 0) {
     movieNameInput.val("");
     movieNameInput.attr("placeholder", "movie name")
+    cardContainer.empty();
     addMovieCard(movieData.results[0]);
+    currentMovieData = movieData.results[0];
   }
   else {
     movieNameInput.val("");
@@ -71,8 +77,62 @@ function addMovieCard(movieData) {
             <li>User Rating: ${movieData.vote_average.toFixed(1)}</li>
             <li>Total User Reviews: ${movieData.vote_count}</li>
           </ul>
-          <a href="#" class="btn btn-primary">Add to Watchlist</a>
+          <a href="#" class="btn btn-primary watchlist-btn">Add to Watchlist</a>
         </div>
       </div>
     `));
+}
+
+function populateCarousel(movieData) {
+  carouselContainer.append($(`      
+  <div id="trending-carousel" class="carousel slide ms-4"  data-bs-ride="carousel" style="width: 30rem;">
+    <div class="carousel-inner">
+      <div class="carousel-item active w-100">
+        <img class="d-block w-100" src="${"https://image.tmdb.org/t/p/original/" + movieData[0].poster_path}" alt="No poster found!">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#trending-carousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#trending-carousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>`));
+
+  const carouselInner = $(".carousel-inner");
+
+  for (var i = 1; i < movieData.length; i++) {
+    carouselInner.append($(`
+      <div class="carousel-item w-100">
+        <img class="d-block w-100" src="${"https://image.tmdb.org/t/p/original/" + movieData[i].poster_path}" alt="No poster found!">
+      </div>`))
+  }
+}
+
+body.on("click", ".watchlist-btn", onWatchlistBtnClick)
+
+function onWatchlistBtnClick() {
+  saveToLocalStorage(currentMovieData);
+}
+movieArray = []
+
+function getFromLocalStorage() {
+  var array = localStorage.getItem("array");
+  if (array) {
+    array = JSON.parse(movieArray);
+  }
+
+  else {
+    array = [];
+  }
+  return array;
+}
+
+
+function saveToLocalStorage(movieArray) {
+  // Below: put a lot of info and put into a string (stringify).
+  localStorage.setItem("array", JSON.stringify(movieArray));
+
 }
